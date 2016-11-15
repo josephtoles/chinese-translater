4657c64fa570d9f8ab04934d54d0d6cff1ebc06e
josephtoles <jbtoles@gmail.com>
2016-11-14 22:40:19 -0800

    Translater works

---
+++ translate.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import os 
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+FILENAME = 'cedict_ts.u8'
+cedict_file_path = dir_path + '/' + FILENAME
+known_words_file_path = dir_path + '/' + 'known_words.txt'
+
+line_count = 0
+
+words = {}
+
+# Parses dictionary file
+print('starting')
+with open(cedict_file_path) as f:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        line_count = line_count + 1
+
+        new_word = {}
+
+        space_index = line.index(' ')
+        traditional = line[:space_index]
+        new_word['traditional'] = traditional
+
+        line = line[space_index+1:]
+
+        space_index = line.index(' ')
+        simplified = line[:space_index]
+        new_word['simplified'] = simplified
+
+        line = line[space_index+1:]
+
+        pinyin_start = line.index('[')
+        pinyin_end = line.index(']')
+        pinyin = line[pinyin_start+1:pinyin_end]
+        new_word['pinyin'] = pinyin
+
+        line = line[pinyin_end+1:]
+
+        definition = line
+        new_word['definition'] = definition
+
+        words[simplified] = new_word
+
+print('loaded words')
+
+# Parses known words file
+known_words = set([])
+with open(known_words_file_path) as f:
+    for line in f:
+        known_words.add(line[:len(line)-1])
+
+print ('loaded known words')
+print ('known words is ' + str(known_words))
+
+#print('words.keys() is ' + str(words.keys()))
+
+# handles translation
+for line in sys.stdin:
+    index = 0
+    line = line[:len(line)-1] # remove trailing return
+    while(index < len(line)):
+        # just using word length of one. You can handle longer words later
+        word_length = 6
+        found = False
+        for word_length in range(1,6)[::-1]:
+            token = line[index:index+word_length]
+            if token in words.keys():
+                index += word_length
+                if token not in known_words:
+                    word = words[token]
+                    print(word['simplified'] + ' ' + word['pinyin'] + ' ' + word['definition'])
+                break
+            if (word_length == 1):
+                index += 1
